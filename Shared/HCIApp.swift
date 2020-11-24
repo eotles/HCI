bc6f2717c2d8dc67692f84f28b11eb639be4a60b
Erkin Otles <eotles@gmail.com>
2020-11-24 16:17:13 -0500

    Initial Commit

---
+++ Shared/HCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HCIApp.swift
+//  Shared
+//
+//  Created by Erkin Otles on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct HCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
